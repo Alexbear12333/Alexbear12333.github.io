a5ad6a7b8c7b0f02311e6166fc64c90c024b15b2
8p_pro <hello@8ppppppppp.tw>
2022-03-31 22:41:47 +0800

    test

---
+++ 41/haha_files/index.php
@@ -0,0 +1,10 @@
+var truvid_cb_124760885837607_loaded = false;
+(function() {
+var currentScript = document.currentScript || document.scripts[document.scripts.length - 1];
+var doc = document;
+var win = window;
+    function insideFriendlyIframe(){try{if(window.frameElement&&window.parent.document)return!0}catch(n){return!1}return!1}
+    if(insideFriendlyIframe()){doc=window.parent.document,win=window.parent.window;var p=document.createElement("div");p.className="truvid_placeholder_3272",window.frameElement.parentNode.insertBefore(p,window.frameElement),window.frameElement.style.height="1px";}
+loadJSFile=function(e,n,l){if(!1!==e){var a=n.createElement("script");a.src=e,a.async=!0,a.src=a.src + "?vid=3d11d3dd536b161b1b69ec49772d22f5c4068ab9" ,a.onload=function(){a.onload=null,l()},currentScript.parentNode.insertBefore(a, currentScript.nextSibling)}else l&&l()};
+if(truvid_cb_124760885837607_loaded == false) {truvid_cb_124760885837607_loaded = true;loadJSFile('//go.trvdp.com/init/3272.js',doc,function() {}); };
+})();
